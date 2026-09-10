1049deb6dff1e2debfbd3462b47bdfc8d5ce63d1
Reivan07 <anthonykhouw07@gmail.com>
2026-09-10 19:46:13 +0700

    versi 2

---
+++ ifnloop.dart
@@ -0,0 +1,68 @@
+void main () {
+    int nilai = 85;
+    // if (niilai > 85) {
+    //     print('nilai di atas 85');
+    // 
+    
+    // if (niilai > 85) {
+    //     print('nilai di atas 85');
+    // } else {
+    //     print('nilai dibawah 85')
+    // }
+    // if (nilai > 85) {
+    //     print('nilai di atas 85');
+
+    // }
+    // if (nilai < 85) {
+    //     print('nilai dibawah 85');
+    // }
+    //switch
+    // var nilai ='A';
+    // switch (nilai) {
+    //     case 'A':
+    //         print('nilai diatas adalah A');
+    //         break;
+    //          case 'B':
+    //         print('nilai diatas adalah B');
+    //         break;
+    //          case 'C':
+    //         print('nilai diatas adalah C');
+    //         break;
+    //         default:
+    //         print ('nilai diluar pilihan');
+    // }
+
+    //for
+    //for in
+    //foreach
+    //while
+    //dowhile
+
+    // for (var i= 0; i < 10; i++) {
+    //     print('nilai ke $i');
+    // }
+    // list datamhs = ['Deni', 'Windy', 'Sindy', 'Michael', 'Jonathan'];
+    // for (var i = 0; i < datamhs.length; i++) {
+    //     print ('nama mhs = ${datamhs[i]}');
+    // }
+
+//  // if (==0) {
+//  // print ('ini deni');
+//  // }
+
+    //for in
+    // list datamhs = ['Deni', 'Windy', 'Sindy', 'Michael', 'Jonathan'];
+    // for (var element in datamhs) {
+    //     print(element);
+    // }
+
+    //foreach
+    list datamhs = ['Deni', 'Windy', 'Sindy', 'Michael', 'Jonathan'];
+
+    // datamhs.forEach((a) {
+    //     //print(a);
+    //     if (a == 'Deni') {
+    //         print(a);
+    //     }
+    // });
+}
