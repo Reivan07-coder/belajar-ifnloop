610af32bc744a55a1fd9eafb0c00ac79cce63a31
Reivan07 <anthonykhouw07@gmail.com>
2026-09-10 20:03:29 +0700

    versi 3

--- ifnloop.dart
+++ ifnloop.dart
@@ -1,68 +1,92 @@
-void main () {
-    int nilai = 85;
-    // if (niilai > 85) {
-    //     print('nilai di atas 85');
-    // 
-    
-    // if (niilai > 85) {
-    //     print('nilai di atas 85');
-    // } else {
-    //     print('nilai dibawah 85')
-    // }
-    // if (nilai > 85) {
-    //     print('nilai di atas 85');
+void main() {
+  int nilai = 85;
+  // if (nilai > 85) {
+  //   print('nilai di atas 85');
+  // }
 
-    // }
-    // if (nilai < 85) {
-    //     print('nilai dibawah 85');
-    // }
-    //switch
-    // var nilai ='A';
-    // switch (nilai) {
-    //     case 'A':
-    //         print('nilai diatas adalah A');
-    //         break;
-    //          case 'B':
-    //         print('nilai diatas adalah B');
-    //         break;
-    //          case 'C':
-    //         print('nilai diatas adalah C');
-    //         break;
-    //         default:
-    //         print ('nilai diluar pilihan');
-    // }
+  //
 
-    //for
-    //for in
-    //foreach
-    //while
-    //dowhile
+  // if (nilai > 85) {
+  //   print('nilai di atas 85');
+  // } else {
+  //   print('nilai dibawah 85');
+  // }
+  // if (nilai > 85) {
+  //     print('nilai di atas 85');
 
-    // for (var i= 0; i < 10; i++) {
-    //     print('nilai ke $i');
-    // }
-    // list datamhs = ['Deni', 'Windy', 'Sindy', 'Michael', 'Jonathan'];
-    // for (var i = 0; i < datamhs.length; i++) {
-    //     print ('nama mhs = ${datamhs[i]}');
-    // }
+  // }
+  // if (nilai < 85) {
+  //     print('nilai dibawah 85');
+  // }
+  //switch
+  // var nilai ='A';
+  // switch (nilai) {
+  //     case 'A':
+  //         print('nilai diatas adalah A');
+  //         break;
+  //          case 'B':
+  //         print('nilai diatas adalah B');
+  //         break;
+  //          case 'C':
+  //         print('nilai diatas adalah C');
+  //         break;
+  //         default:
+  //         print ('nilai diluar pilihan');
+  // }
 
-//  // if (==0) {
-//  // print ('ini deni');
-//  // }
+  //for
+  //for in
+  //foreach
+  //while
+  //dowhile
 
-    //for in
-    // list datamhs = ['Deni', 'Windy', 'Sindy', 'Michael', 'Jonathan'];
-    // for (var element in datamhs) {
-    //     print(element);
-    // }
+  // for (var i= 0; i < 10; i++) {
+  //     print('nilai ke $i');
+  // }
+  // List<String> datamhs = ['Deni', 'Windy', 'Sindy', 'Michael', 'Jonathan'];
+  // for (var i = 0; i < datamhs.length; i++) {
+  //     print ('nama mhs = ${datamhs[i]}');
+  // }
 
-    //foreach
-    list datamhs = ['Deni', 'Windy', 'Sindy', 'Michael', 'Jonathan'];
+  //  // if (==0) {
+  //  // print ('ini deni');
+  //  // }
 
-    // datamhs.forEach((a) {
-    //     //print(a);
-    //     if (a == 'Deni') {
-    //         print(a);
-    //     }
-    // });
+  //for in
+  // list datamhs = ['Deni', 'Windy', 'Sindy', 'Michael', 'Jonathan'];
+  // for (var element in datamhs) {
+  //     print(element);
+  // }
+
+  //foreach
+  //List<String> datamhs = ['Deni', 'Windy', 'Sindy', 'Michael', 'Jonathan'];
+
+  // datamhs.forEach((a) {
+  //     //print(a);
+  //     if (a == 'Deni') {
+  //         print(a);
+  //     }
+  // });
+
+  //while
+  // var nilai =0;
+
+  // while (nilai < 5) {
+  //     print (nilai);
+  //     nilai++;
+  //     }
+  // list datamhs = ['Deni', 'Windy', 'Sindy', 'Michael', 'Jonathan'];
+  // var index = 0;
+  // while (index < datamhs.length) {
+  //     print(datamhs[index]);
+  //     index++;
+  //}
+
+  //do while
+  // list datamhs = ['Deni', 'Windy', 'Sindy', 'Michael', 'Jonathan'];
+  // var index = 0;
+  // do {
+  //     print(datamhs[index]);
+  //     index++;
+  // } while (index < datamhs.length);
 }
